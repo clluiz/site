0502188c477c32f299a5f58146eed8371ce00a93
Cleiton Luiz <cleiton.vanquish@gmail.com>
2022-09-01 18:44:40 -0300

    inclusão do eleventy

---
+++ .eleventy.js
@@ -0,0 +1,10 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("/css/");
+  eleventyConfig.addWatchTarget("/css/");
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    }
+  }
+}
