61605fa62b3a7e0525ce8884e5b0427171015c7a
Cleiton Luiz <cleiton.vanquish@gmail.com>
2022-09-08 18:15:41 -0300

    exibe os posts do blog

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("/css/");
   eleventyConfig.addWatchTarget("/css/");
   return {
+    markdownTemplateEngine: "njk",
+    templateFormats: ["html", "njk", "md"],
     dir: {
       input: "src",
       output: "public",
