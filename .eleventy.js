dc4224de36e0be12429a77190e44db9ae3d55015
Cleiton Luiz <cleiton.vanquish@gmail.com>
2022-09-15 18:22:03 -0300

    inclui tailwind no projeto

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addWatchTarget("src/css");
+  eleventyConfig.addWatchTarget('./tailwind.config.js');
+  
   return {
     markdownTemplateEngine: "njk",
     templateFormats: ["html", "njk", "md"],
