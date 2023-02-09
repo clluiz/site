bbe2e2c04f740b454a90e698e3f8165e45f489f2
Cleiton Luiz <cleiton.dev@proton.me>
2023-02-09 13:38:23 -0300

    internacionalização da página inicial

---
+++ src/_data/i18n/index.js
@@ -0,0 +1,74 @@
+module.exports = {
+    greetings: {
+        'en-US': 'Hi, my name is Cleiton Luiz',
+        'pt-BR': 'Olá, meu nome é Cleiton Luiz'
+    },
+    what_i_do: {
+        'en-US': 'I deliver efficient and creative technology solutions to agencies, maximizing results with top-quality programming. ',
+        'pt-BR': 'Ofereço soluções tecnológicas eficientes e criativas para sua empresa, maximizando resultados com programação de alta qualidade'
+    },
+    catch_question: {
+        'en-US': 'Does your company need a web/mobile developer?',
+        'pt-BR': 'A sua empresa precisa de um desenvolvedor web/mobile?'
+    },
+    experience: {
+        'en-US': 'I have over 10 years of experience developing websites and web applications. Working on the frontend and backend.',
+        'pt-BR': 'Tenho com mais de 10 anos de experiência desenvolvendo sites e aplicações web. Atuando no frontend e backend.'
+    },
+    skills: {
+        'en-US': 'If your company or clients have projects involving javascript, nodejs, react, react native, vue, python. I can help you!',
+        'pt-BR': 'Se sua empresa ou clientes tem projetos que envolvam javascript, nodejs, react, react native, vue, python. Eu posso te ajudar!'
+    },
+    services: {
+        'en-US': 'Services',
+        'pt-BR': 'Serviços'
+    },
+    blog: {
+        'en-US': 'Blog',
+        'pt-BR': 'Blog'
+    },
+    portfolio: {
+        'en-US': 'Portfolio',
+        'pt-BR': 'Portfólio'
+    },
+    service_sites: {
+        'en-US': 'Websites that convert visitors into customers',
+        'pt-BR': 'Sites que transformam visitantes em clientes'
+    },
+    service_e_commerce: {
+        'en-US': 'Integration of e-commerce platforms, payment methods, custom templates',
+        'pt-BR': 'Integração de plataformas de e-commerce, meios de pagamento, templates customizados'
+    },
+    service_app: {
+        'en-US': 'Web and mobile applications tailored to your business needs',
+        'pt-BR': 'Aplicativos para web e dispositivos móveis de acordo com as necessidades da sua empresa'
+    },
+    worked_companies: {
+        'en-US': 'Companies I have provided services to',
+        'pt-BR': 'Empresas para as quais já prestei serviços'
+    },
+    contact: {
+        'en-US': 'Contact',
+        'pt-BR': 'Contato'
+    },
+    get_in_touch: {
+        'en-US': 'contact me!',
+        'pt-BR': 'Entre em contato comigo'
+    },
+    testimonials: {
+        'en-US': 'Testimonials',
+        'pt-BR': 'Testemunhos'
+    },
+    social_networks: {
+        'en-US': 'Social',
+        'pt-BR': 'Redes sociais'
+    },
+    site_map: {
+        'en-US': 'Site map',
+        'pt-BR': 'Mapa do site'
+    },
+    applications: {
+        'en-US': 'Apps',
+        'pt-BR': 'Aplicativos'
+    }
+};
---
+++ src/posts/rpa-pequenas-medias-empresas.md
@@ -0,0 +1,23 @@
+---
+layout: post.njk
+permalink: /blog/rpa-pequenas-medias-empresas.html
+date: 2023-02-09
+description: Como o uso de RPA pode melhorar a produtivdade da sua empresa
+title: RPA para pequenas e médias empresas
+---
+
+# Benefícios do uso de RPA para pequenas e médias empresas
+
+Robotic Process Automation (RPA) é uma tecnologia de automação de processos baseada em software que permite a automação de tarefas repetitivas e manuais, liberando tempo e recursos para tarefas mais estratégicas. Nos últimos anos, muitas pequenas e médias empresas no Brasil têm adotado a RPA como uma forma de aumentar a eficiência, reduzir custos e melhorar a qualidade dos serviços oferecidos aos clientes.
+
+Aqui estão alguns dos principais benefícios da RPA para pequenas e médias empresas no Brasil:
+
+1. Redução de custos: A automação de tarefas manuais permite às empresas reduzir significativamente os custos com mão de obra, já que os robôs podem realizar tarefas 24 horas por dia, 7 dias por semana, sem interrupções ou erros humanos.
+
+2. Melhoria da eficiência: A RPA permite a automação de processos de negócios complexos, o que resulta em uma melhoria na velocidade e eficiência dos processos. Além disso, a RPA também permite uma melhor integração entre diferentes sistemas e aplicações, o que resulta em uma melhoria geral da eficiência dos processos.
+
+3. Aumento da qualidade dos serviços: A RPA permite que as empresas ofereçam serviços mais precisos e confiáveis aos seus clientes, já que os robôs são menos propensos a erros humanos e podem realizar tarefas de maneira mais consistente e precisa.
+
+4. Liberação de tempo para tarefas mais estratégicas: Ao automatizar tarefas manuais, a RPA permite que os funcionários se concentrem em tarefas mais estratégicas e críticas para o negócio, o que resulta em uma melhoria geral na eficiência e eficácia da empresa.
+
+Em resumo, a RPA é uma ferramenta poderosa que pode oferecer muitos benefícios para pequenas e médias empresas no Brasil. A automação de processos de negócios pode ajudar a aumentar a eficiência, reduzir custos, melhorar a qualidade dos serviços e liberar tempo para tarefas mais estratégicas.
